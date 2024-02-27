34c6352a2b94f9a5001790e0b346eda1333853d7
Your Name <you@example.com>
2024-02-27 19:38:59 +0000

    init

---
+++ src/config.js
@@ -0,0 +1,6 @@
+const BASE_URL          = "https://api.themoviedb.org/3/";
+const API_KEY_PARAM     = "?api_key=ceec4e7b4d5d071195fa1515586af276";
+const BACKDROP_BASE_URL = "https://image.tmdb.org/t/p/original/";
+const SMALL_IMG_COVER_BASE_URL = "https://image.tmdb.org/t/p/w300/";    
+
+export { BASE_URL, API_KEY_PARAM, BACKDROP_BASE_URL, SMALL_IMG_COVER_BASE_URL };
